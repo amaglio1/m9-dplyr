fc7900123551e6b8d29742df9b098ee6bd7c594f
Anna <amaglio@uw.edu>
2016-10-18 15:11:23 -0700

    exercise-3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -9,10 +9,13 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
+data("vehicles")
 
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+acura.vehicles <- vehicles[vehicles$make == 'Acura',]
+acura.2015 <- acura.vehicles[acura.vehicles$year == 2015,]
+best <- acura.2015[acura.2015$hwy == max(acura.2015$hwy),3]
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
 
